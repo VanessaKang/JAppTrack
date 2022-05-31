a214f29a6a094ec2f4d265fd1ce4e9aa847b3320
vanessa <VanessaKang@users.noreply.github.com>
2022-05-31 00:29:56 -0400

    Added validation methods for Interview model

--- app/models/interview.py
+++ app/models/interview.py
@@ -1,7 +1,10 @@
+from operator import and_
 from app import db
-from app.models import BaseModel, application
+from app.models import BaseModel, ModelValidationException, application
 from app.models.user import User
+from sqlalchemy.orm import validates, and_
 from enum import Enum
+
 
 
 class InterviewTypeEnum(Enum):
@@ -25,13 +28,25 @@ class Interview(BaseModel):
     __tablename__ = "interview"
 
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime)
+    scheduled_date = db.Column(db.DateTime)
     status = db.Column(db.Enum(InterviewStatusEnum), nullable=False, default=InterviewStatusEnum.pending)
     type = db.Column(db.Enum(InterviewTypeEnum), nullable=False)
     notes = db.Column(db.Text)
 
     application_id = db.Column(db.Integer, db.ForeignKey('application.id'), nullable=False)
     application = db.relationship('Application', backref=db.backref('interviews', lazy=True))
+
+    @validates("status")
+    def validate_interview_status(self, key, status):
+        if status != InterviewStatusEnum.pending and self.scheduled_date is None:
+            raise ModelValidationException("Date required for scheduled interviews.")
+        return status
+    
+    @validates("application")
+    def validate_scheduled_date(self, key, application):
+        if Interview.query.filter(and_(Interview.application == application, Interview.scheduled_date >= self.scheduled_date)).all():
+            raise ModelValidationException("This interview cannot be scheduled before previous interviews.")
+        return application
 
     @classmethod
     def find_by_status(cls, status):
